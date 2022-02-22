939e4c679873b3ff863b3f3680e831a9ecb188ea
Victor Martinez <victormartinezg1998@gmail.com>
2022-02-21 19:34:39 -0500

    Preparando deploy

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/registro-clientes'));
+app.use(express.static('/dist/registro-clientes'));
 
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: '/dist/registro-clientes/'}),
